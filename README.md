dd7274c58b6eb7709d8d6c8fcbbcf1b0cb0340f9
DA CUNHA Mario <dacunhamario@outlook.com>
2023-05-31 16:59:59 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# LOG-680 : Template for Oxygen-CS
+
+This Python application continuously monitors a sensor hub and manages HVAC (Heating, Ventilation, and Air Conditioning) system actions based on received sensor data.
+
+It leverages `signalrcore` to maintain a real-time connection to the sensor hub and utilizes `requests` to send GET requests to a remote HVAC control endpoint.
+
+This application uses `pipenv`, a tool that aims to bring the best of all packaging worlds to the Python world.
+
+## Requierements
+
+- Python 3.8+
+- pipenv
+
+## Getting Started
+
+1. Clone the repository :
+
+```
+git clone https://github.com/your-username/sensor-hub-monitor.git
+cd sensor-hub-monitor
+```
+
+2. Install the project's dependencies :
+
+```
+pipenv install
+```
+
+## Setup
+
+You need to setup the following variables inside the Main class:
+
+- HOST: The host of the sensor hub and HVAC system.
+- TOKEN: The token for authenticating requests.
+- TICKETS: The number of tickets.
+- T_MAX: The maximum allowed temperature.
+- T_MIN: The minimum allowed temperature.
+- DATABASE: The database connection details.
+
+## Running the Program
+
+After setup, you can start the program with the following command:
+
+```
+pipenv run strart
+```
+
+## Logging
+
+The application logs important events such as connection open/close and error events to help in troubleshooting.
+
+## To Implement
+
+There are placeholders in the code for sending events to a database and handling request exceptions. These sections should be completed as per the requirements of your specific application.
+
+## License
+
+MIT
+
+## Contact
+
+For more information, please feel free to contact the repository owner.
---
+++ src/main.py
@@ -0,0 +1,86 @@
+from signalrcore.hub_connection_builder import HubConnectionBuilder
+import logging
+import requests
+import json
+import time
+
+
+class Main:
+    def __init__(self):
+        self._hub_connection = None
+        self.HOST = None  # Setup your host here
+        self.TOKEN = None  # Setup your token here
+        self.TICKETS = None  # Setup your tickets here
+        self.T_MAX = None  # Setup your max temperature here
+        self.T_MIN = None  # Setup your min temperature here
+        self.DATABASE = None  # Setup your database here
+
+    def __del__(self):
+        if self._hub_connection != None:
+            self._hub_connection.stop()
+
+    def setup(self):
+        self.setSensorHub()
+
+    def start(self):
+        self.setup()
+        self._hub_connection.start()
+
+        print("Press CTRL+C to exit.")
+        while True:
+            time.sleep(2)
+
+    def setSensorHub(self):
+        self._hub_connection = (
+            HubConnectionBuilder()
+            .with_url(f"{self.HOST}/SensorHub?token={self.TOKEN}")
+            .configure_logging(logging.INFO)
+            .with_automatic_reconnect(
+                {
+                    "type": "raw",
+                    "keep_alive_interval": 10,
+                    "reconnect_interval": 5,
+                    "max_attempts": 999,
+                }
+            )
+            .build()
+        )
+
+        self._hub_connection.on("ReceiveSensorData", self.onSensorDataReceived)
+        self._hub_connection.on_open(lambda: print("||| Connection opened."))
+        self._hub_connection.on_close(lambda: print("||| Connection closed."))
+        self._hub_connection.on_error(lambda data: print(f"||| An exception was thrown closed: {data.error}"))
+
+    def onSensorDataReceived(self, data):
+        try:
+            print(data[0]["date"] + " --> " + data[0]["data"])
+            date = data[0]["date"]
+            dp = float(data[0]["data"])
+            self.send_temperature_to_fastapi(date, dp)
+            self.analyzeDatapoint(date, dp)
+        except Exception as err:
+            print(err)
+
+    def analyzeDatapoint(self, date, data):
+        if float(data) >= float(self.T_MAX):
+            self.sendActionToHvac(date, "TurnOnAc", self.TICKETS)
+        elif float(data) <= float(self.T_MIN):
+            self.sendActionToHvac(date, "TurnOnHeater", self.TICKETS)
+
+    def sendActionToHvac(self, date, action, nbTick):
+        r = requests.get(f"{self.HOST}/api/hvac/{self.TOKEN}/{action}/{nbTick}")
+        details = json.loads(r.text)
+        print(details)
+
+    def send_event_to_database(self, timestamp, event):
+        try:
+            # To implement
+            pass
+        except requests.exceptions.RequestException as e:
+            # To implement
+            pass
+
+
+if __name__ == "__main__":
+    main = Main()
+    main.start()
